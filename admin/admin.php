13c0af350f82313a3158ff2c856286dd6687b602
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-24 19:48:58 +0900

    :sparkles: [add] 管理画面用のスタイルシートを読み込む処理を追加
    ■ 管理画面用のスタイルシートを読み込む処理を追加した。
      ・ [admin_enqueue_scripts]アクションフックによりスタイルシートを読み込む。

--- admin/admin.php
+++ admin/admin.php
@@ -6,6 +6,7 @@ require_once NT_WPCF7SN_PLUGIN_DIR . '/admin/includes/contact-forms-list-table.p
 
 
 add_action( 'admin_menu', 'nt_wpcf7sn_admin_menu' );
+add_action( 'admin_enqueue_scripts', 'nt_wpcf7sn_admin_enqueue_scripts' );
 
 
 function nt_wpcf7sn_admin_menu() {
@@ -13,7 +14,7 @@ function nt_wpcf7sn_admin_menu() {
 		__( 'Contact Form 7 Serial Number Addon' ),
 		__( 'CF7 Serial Number' ),
 		'manage_options',
-		'wpcf7sn',
+		'nt-wpcf7sn',
 		'nt_wpcf7sn_admin_management_page'
 	);
 }
@@ -30,4 +31,18 @@ function nt_wpcf7sn_admin_management_page() {
 	$list_table = new NT_WPCF7SN_Contact_Forms_List_Table();
 	$list_table->prepare_items();
 	$list_table->display();
+}
+
+
+function nt_wpcf7sn_admin_enqueue_scripts( $hook_suffix ) {
+	if ( false === strpos( $hook_suffix, 'nt-wpcf7sn' ) ) {
+		return;
+	}
+
+	wp_enqueue_style(
+		NT_WPCF7SN_TEXT_DOMAIN . '-admin',
+		NT_WPCF7SN_PLUGIN_URL . '/admin/css/style.css',
+		array(),
+		NT_WPCF7SN_VERSION, 'all'
+	);
 }
