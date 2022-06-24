3ace50feb076b3a7d072efe4a731f00c0b5fe34d
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-24 14:00:53 +0900

    :memo: [docs] プラグイン情報のヘッダーフィールドを作成
    ■ WordPressに通知されるプラグイン情報のヘッダーフィールドを作成した。
    
    ◆ Plugin Developer Handbook | ヘッダー要件
    <https://developer.wordpress.org/plugins/plugin-basics/header-requirements/>

---
+++ contact-form-7-serial-number.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Contact Form 7 Serial Number Addon
+Plugin URI: 
+Description: 
+Author: Nobuntu
+Author URI: https://profiles.wordpress.org/nobuntu87/
+Text Domain: contact-form-7-serial-number
+Domain Path: /languages/
+Version: 0.0.0
+License: GPL2+ (GNU General Public License v2 or later)
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
