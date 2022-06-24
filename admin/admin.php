8fdfb66924b7dd7202800affee60a1f8cdbc0785
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-24 14:07:46 +0900

    :sparkles: [add] プラグイン管理画面の生成処理を追加
    ■ プラグインの管理設定画面のページを生成する処理を追加した。
      ・ [admin_menu]アクションフックにより管理画面を生成する。
    
    ■ プラグインの名称・パス関連の定数定義を追加した。

---
+++ admin/admin.php
@@ -0,0 +1,26 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+add_action( 'admin_menu', 'nt_wpcf7sn_admin_menu' );
+
+
+function nt_wpcf7sn_admin_menu() {
+	add_options_page(
+		__( 'Contact Form 7 Serial Number Addon' ),
+		__( 'CF7 Serial Number' ),
+		'manage_options',
+		'wpcf7sn',
+		'nt_wpcf7sn_admin_management_page'
+	);
+}
+
+
+function nt_wpcf7sn_admin_management_page() {
+	$output = ''
+	. '<div class="wrap">'
+	. '  <h2>Contact Form 7 Serial Number Addon</h2>'
+	. '</div>';
+
+	echo trim( $output );
+}
---
+++ load.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+if ( is_admin() ) {
+	require_once NT_WPCF7SN_PLUGIN_DIR . '/admin/admin.php';
+}
