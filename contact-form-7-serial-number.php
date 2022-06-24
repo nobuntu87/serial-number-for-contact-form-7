ac9373f49a718f62abdb7f2d50010332b6b2c3cb
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-24 19:48:37 +0900

    :sparkles: [add] 管理画面用のスタイルシートを読み込む処理を追加
    ■ 管理画面用のスタイルシートを読み込む処理を追加した。
      ・ [admin_enqueue_scripts]アクションフックによりスタイルシートを読み込む。

--- contact-form-7-serial-number.php
+++ contact-form-7-serial-number.php
@@ -15,6 +15,8 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 
+define( 'NT_WPCF7SN_VERSION', '0.0.0' );
+
 // [contact-form-7-serial-number]
 define( 'NT_WPCF7SN_TEXT_DOMAIN', 'contact-form-7-serial-number' );
 
@@ -30,5 +32,7 @@ define( 'NT_WPCF7SN_PLUGIN_NAME', trim( dirname( NT_WPCF7SN_PLUGIN_BASENAME ), '
 // [\~\wp-content\plugins\contact-form-7-serial-number]
 define( 'NT_WPCF7SN_PLUGIN_DIR', untrailingslashit( dirname( NT_WPCF7SN_PLUGIN ) ) );
 
+// [//~/wp-content/plugins/contact-form-7-serial-number]
+define( 'NT_WPCF7SN_PLUGIN_URL', untrailingslashit( plugins_url( '', NT_WPCF7SN_PLUGIN ) ) );
 
 require_once NT_WPCF7SN_PLUGIN_DIR . '/load.php';
