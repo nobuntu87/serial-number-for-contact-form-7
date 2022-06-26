2e12c645e5f31c11e5b6f1e07d4c1f89d814b148
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-26 19:02:17 +0900

    :sparkles: [add] タイムゾーンを設定する処理を追加
    ■ プラグイン初期化時にタイムゾーンを設定する処理を追加した。
      ・ [init]アクションフックにより初期化処理を行う。
      ・ タイムゾーン設定はWordPressの設定から読み込む。

--- load.php
+++ load.php
@@ -5,3 +5,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if ( is_admin() ) {
 	require_once NT_WPCF7SN_PLUGIN_DIR . '/admin/admin.php';
 }
+
+
+add_action( 'init', 'nt_wpcf7sn_init' );
+
+
+function nt_wpcf7sn_init() {
+	$timezone = get_option( 'timezone_string', 'UTC' );
+	date_default_timezone_set( $timezone );
+}
