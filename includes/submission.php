f1ce09ded238df5b181709522f704c89e1674b40
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-30 15:59:52 +0900

    :sparkles: [add] フォーム送信後にカウント値を更新する
    ■ フォーム送信後に対象フォームのカウント値をカウントアップする処理を追加した。
      ・ [wpcf7_submit]フックにより処理を行う。
      ・ 送信の成功/失敗に関わらずカウントアップを行う。(Flamingoと同じ設計方針)

--- includes/submission.php
+++ includes/submission.php
@@ -2,7 +2,15 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 
+add_action( 'wpcf7_submit', 'nt_wpcf7sn_submit', 10, 2 );
+
 add_filter( 'wpcf7_posted_data', 'nt_wpcf7sn_posted_data', 10, 1 );
+
+
+function nt_wpcf7sn_submit( $contact_form, $result ) {
+	$form_id = intval( $contact_form->id );
+	nt_wpcf7sn_increment_count( $form_id );
+}
 
 
 function nt_wpcf7sn_posted_data( $posted_data ) {
@@ -20,4 +28,10 @@ function nt_wpcf7sn_posted_data( $posted_data ) {
 	}
 
 	return $posted_data;
+}
+
+
+function nt_wpcf7sn_increment_count( $form_id ) {
+	$count = intval( get_option( 'nt_wpcf7sn_count_' . $form_id ) );
+	update_option( 'nt_wpcf7sn_count_' . $form_id , $count + 1 );
 }
