4804beefc68fef28225d288ca7dace03b8bf0f41
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-06-30 15:56:33 +0900

    :sparkles: [add] フォーム送信時のPOSTデータにシリアル番号を保存
    ■ フォーム送信時のPOSTデータにシリアル番号を生成し保存する処理を追加した。
      ・ [wpcf7_posted_data]フックにより処理を行う。

--- includes/submission.php
+++ includes/submission.php
@@ -1,2 +1,23 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+add_filter( 'wpcf7_posted_data', 'nt_wpcf7sn_posted_data', 10, 1 );
+
+
+function nt_wpcf7sn_posted_data( $posted_data ) {
+
+	if ( class_exists( 'WPCF7_Submission' ) ) {
+
+		$submission = WPCF7_Submission::get_instance();
+		
+		$contact_form = $submission->get_contact_form();
+		$form_id = intval( $contact_form->id );
+
+		$count = intval( get_option( 'nt_wpcf7sn_count_' . $form_id ) );
+
+		$posted_data['serial-number'] = NT_WPCF7SN_Serial_Number::get_serial_number( $form_id, $count + 1 );
+	}
+
+	return $posted_data;
+}
