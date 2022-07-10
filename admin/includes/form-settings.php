389305d684081c751c9b1fdc8ecaa43fc909db90
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-07-10 11:49:15 +0900

    :heavy_plus_sign: [add] 設定項目とサニタイズ処理の登録を追加
    ■ 設定項目とサニタイズ処理の登録を追加した。
      ・ Wordpressの「Settings API」の機能を使用する。

--- admin/includes/form-settings.php
+++ admin/includes/form-settings.php
@@ -1,2 +1,28 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * アクションフック設定
+ */
+add_action( 'admin_init', 'nt_wpcf7sn_register_setting', 10, 0 );
+
+
+/**
+ * 設定項目と無害化用コールバックを登録する。
+ *
+ * @return void
+ */
+function nt_wpcf7sn_register_setting() {
+
+}
+
+
+/**
+ * コンタクトフォームのオプションのサニタイズ処理を行う。
+ *
+ * @param mixed[] $options 入力されたオプション値
+ * @return $options サニタイズ処理したオプション値を返す。
+ */
+function nt_wpcf7sn_sanitize_form_options( $options ) {
+
+}
