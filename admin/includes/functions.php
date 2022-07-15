b77c65bc2a9ffd438b21416f72d9fb0e19d26b6d
nobuntu87 <nobuntu.media.studio@gmail.com>
2022-07-15 18:57:35 +0900

    :sparkles: [add] 外部プラグインの有効化チェック処理を追加
    ■ 外部プラグインの有効化チェック処理を追加した。
     ・ 管理画面のみ使用可能。

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,2 +1,28 @@
 <?php
 if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * ファイル読み込み
+ */
+if ( is_admin() ) {
+	include_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+
+/**
+ * プラグインが有効化されているか確認する。
+ * 
+ * 管理画面のみ使用可能。
+ *
+ * @param string $plugin プラグイン名 (plugin-directory/plugin-file.php)
+ * @return bool プラグインが有効化されている場合はtrueを返す。
+ *              プラグインが有効化されていない場合はfalseを返す。
+ *              メソッド使用不可(管理画面以外)の場合はfalseを返す。
+ */
+function nt_wpcf7sn_is_active_plugin( $plugin ) {
+	if ( function_exists( 'is_plugin_active' ) ) {
+		return is_plugin_active( $plugin );
+	} else {
+		return false;
+	}
+}
