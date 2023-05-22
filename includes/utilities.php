5de079841674f63a83930d8fa4f247d7f745ae4a
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-22 18:48:40 +0900

    :sparkles: [add] ContactForm7 投稿情報の取得機能を追加
    ■ ContactForm7 投稿情報の取得機能を追加した。

--- includes/utilities.php
+++ includes/utilities.php
@@ -3,5 +3,22 @@ namespace _Nt\WpPlg\WPCF7SN;
 if ( !defined( 'ABSPATH' ) ) exit;
 
 class Utility {
-	
+
+	/**
+	 * Contact Form 7 の投稿情報を取得する。
+	 *
+	 * @return WP_Post[] Contact Form 7 の投稿オブジェクトを返す。
+	 */
+	public static function get_wpcf7_posts()
+	{
+		return get_posts( array(
+			'post_type'      => 'wpcf7_contact_form',
+			'post_status'    => 'publish',
+			'orderby'        => 'ID',
+			'order'          => 'ASC',
+			'posts_per_page' => -1,
+			'offset'         => 0,
+		) );
+	}
+
 }
