e26540ffe858b73245cd71df3b642c4646374332
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-23 21:36:38 +0900

    :sparkles: [add] プラグインのメタ情報設定処理を追加
    ■ プラグインのメタ情報設定処理を追加した。

--- admin/includes/plugin.php
+++ admin/includes/plugin.php
@@ -76,4 +76,57 @@ class NT_WPCF7SN_Admin {
 		return $actions;
 	}
 
+	/**
+	 * プラグインのメタ情報を設定する。
+	 * 
+	 * [Filter Hook] plugin_row_meta
+	 *
+	 * @param string[] $plugin_meta メタ情報
+	 * @param string $plugin_file プラグイン名 : {plugin-name}\{main-file.php}
+	 * @return void
+	 */
+	public static function set_plugin_meta_info( $plugin_meta, $plugin_file )
+	{
+		// 自プラグイン判別
+		if ( _PLUGIN_BASENAME != $plugin_file ) { return $plugin_meta; }
+
+		// ------------------------------------
+		// 依存関係チェック：Contact Form 7 プラグイン
+		// ------------------------------------
+
+		// [ContactForm7] 無効化の場合
+		if ( !NT_WPCF7SN::is_active_wpcf7() ) {
+
+			$iframe_url = Utility::get_plugin_iframe_url( _EXTERNAL_PLUGIN['wpcf7']['slug'] );
+
+			$plugin_link = sprintf( ''
+				. '<a href="%s" class="%s" data-title="%s">%s</a>'
+				, esc_url( $iframe_url )
+				, esc_attr( 'thickbox open-plugin-details-modal' )
+				, esc_attr( _EXTERNAL_PLUGIN['wpcf7']['name'] )
+				, esc_attr( _EXTERNAL_PLUGIN['wpcf7']['name'] )
+			);
+
+			$message = sprintf( __( ''
+				. '<strong>Required the %s plugin to work.</strong>'
+				. ' Please install and activate the plugin first.'
+				, _TEXT_DOMAIN )
+				, $plugin_link
+			);
+
+			$meta_info = sprintf( ''
+				. '<div class="%s"><p>%s</p></div>'
+				, esc_attr( 'notice notice-warning notice-alt inline' )
+				, $message
+			);
+
+			// 先頭に登録
+			array_unshift( $plugin_meta, wp_kses_post( $meta_info ) );
+		}
+
+		// ------------------------------------
+
+		return $plugin_meta;
+	}
+
 }
