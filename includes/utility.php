d918040e15d1c3c3131a5e3de150b7c9c5c5b43c
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-23 16:46:27 +0900

    :sparkles: [add] 文字列のエンコード/デコード機能を追加
    ■ 文字列のエンコード/デコード機能を追加した。

--- includes/utility.php
+++ includes/utility.php
@@ -21,4 +21,42 @@ class Utility {
 		) );
 	}
 
+	/**
+	 * 文字列のエスケープ/エンコード処理を行う。
+	 *
+	 * @param string $string 文字列
+	 * @return string エスケープ処理した文字列を返す。
+	 */
+	public static function esc_encode( $string )
+	{
+		if ( !is_string( $string ) ) { return $string; }
+
+		// エンコード
+		$string = htmlspecialchars( $string, ENT_QUOTES, 'UTF-8' );
+
+		// エスケープ
+		$string = addslashes( $string );
+
+		return $string;
+	}
+
+	/**
+	 * 文字列のアンエスケープ/デコード処理を行う。
+	 *
+	 * @param string $string 文字列
+	 * @return string アンエスケープ処理した文字列を返す。
+	 */
+	public static function unesc_decode( $string )
+	{
+		if ( !is_string( $string ) ) { return $string; }
+
+		// アンエスケープ
+		$string = stripslashes( $string );
+
+		// デコード
+		$string = htmlspecialchars_decode( $string, ENT_QUOTES );
+
+		return $string;
+	}
+
 }
