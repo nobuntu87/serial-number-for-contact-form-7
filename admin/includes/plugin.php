e1d12d861122637bcacbe8fe89e089eb740ff10e
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-23 18:38:23 +0900

    :sparkles: [add] プラグインのアクションリンク設定処理を追加
    ■ プラグインのアクションリンク設定処理を追加した。

--- admin/includes/plugin.php
+++ admin/includes/plugin.php
@@ -44,4 +44,36 @@ class NT_WPCF7SN_Admin {
 		SELF::update_option( 'version', $new_version );
 	}
 
+	/**
+	 * プラグインのアクションリンクを設定する。
+	 * 
+	 * [Filter Hook] plugin_action_links
+	 *
+	 * @param string[] $actions アクションリンク
+	 * @param string $plugin_file プラグイン名 : {plugin-name}\{main-file.php}
+	 * @return void アクションリンクを返す。
+	 */
+	function set_plugin_action_links( $actions, $plugin_file )
+	{
+		// 自プラグイン判別
+		if ( _PLUGIN_BASENAME != $plugin_file ) { return $actions; }
+		
+		// ------------------------------------
+		// プラグイン設定ページ登録
+		// ------------------------------------
+
+		$action_link = sprintf( ''
+			. '<a href="%s">%s</a>'
+			, esc_url( menu_page_url( _PREFIX['-'], false ) )
+			, esc_html( __( 'Settings', _TEXT_DOMAIN ) )
+		);
+
+		// 先頭に登録
+		array_unshift( $actions, wp_kses_post( $action_link ) );
+
+		// ------------------------------------
+
+		return $actions;
+	}
+
 }
