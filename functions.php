fe8a67f5d37c12908b40134f7a537e693057352c
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-27 06:33:57 +0900

    :sparkles: [add] デイリーリセット実行チェックのアクションフックを追加
    ■ デイリーリセット実行チェックのアクションフックを追加した。

--- functions.php
+++ functions.php
@@ -22,3 +22,15 @@ add_action(
 	__NAMESPACE__ . '\Form_Option::init_options',
 	10, 0
 );
+
+// デイリーリセット実行確認
+add_action(
+	'init',
+	__NAMESPACE__ . '\NT_WPCF7SN::check_reset_count',
+	11, 0
+);
+add_action(
+	'nt_wpcf7sn_check_reset_count',
+	__NAMESPACE__ . '\NT_WPCF7SN::check_reset_count',
+	10, 0
+);
