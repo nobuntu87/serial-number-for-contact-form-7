938b3b838eee036942616376dd73767311a89697
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-29 02:11:38 +0900

    :sparkles: [add] ユニークIDの生成機能を追加
    ■ ユニークIDの生成機能を追加した。

--- includes/serial-number.php
+++ includes/serial-number.php
@@ -8,4 +8,33 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 class Serial_Number {
 
+	/**
+	 * ユニークIDを生成する。
+	 *
+	 * @param int|string $count メールカウント
+	 * @return string ユニークIDを返す。
+	 */
+	private function create_unique_id( $count )
+	{
+		$unique_id = '';
+
+		// タイムコード (UNIX時間を基準に桁数を減らすため起点時刻を変更)
+		$microtime = microtime( true );
+		$microtime -= strtotime( '2022/01/01 00:00:00' );
+		$microtime = sprintf( '%.4f', $microtime );
+
+		// 乱数 (00~99)
+		$randum = sprintf( '%02d', mt_rand( 0, 99 ) );
+
+		// ユニークIDの算出値を作成 (逆順変換)
+		$basecode = $microtime . intval( $count ) . $randum;
+		$basecode = strrev( $basecode );
+
+		// 10進数を36進数[0-9/a-z]に変換 (大文字変換)
+		$unique_id = base_convert( $basecode, 10, 36 );
+		$unique_id = strtoupper( $unique_id );
+
+		return strval( $unique_id );
+	}
+
 }
