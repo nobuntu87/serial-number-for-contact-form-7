b83473f588f44406ce9b7e55c21ce1f03fb32584
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-29 01:28:01 +0900

    :sparkles: [add] オプション値の個別バリテーション機能を追加
    ■ オプション値の個別バリテーション機能を追加した。

--- includes/form-option.php
+++ includes/form-option.php
@@ -490,6 +490,161 @@ class Form_Validate {
 		return $validity;
 	}
 
+   // ------------------------------------
+   // 個別オプション検証
+   // ------------------------------------
+
+	/**
+	 * オプション値の検証を行う。(プレフィックス)
+	 *
+	 * @param string $key オプションキー
+	 * @param mixed $value オプション値
+	 * @param string|null $message エラーメッセージ
+	 * @return boolean 有効性の検証結果を返す。(true:有効/false:無効)
+	 */
+	public static function validate_prefix( $value, &$message = null )
+	{
+		$validity = empty( $message ) ? true : false ;
+
+		// ------------------------------------
+		// 入力パターン検証
+		// ------------------------------------
+
+		if ( $validity ) {
+			if ( !SELF::is_match_pattern( 'prefix', $value ) ) {
+				$validity = false;
+			}
+		}
+
+		// ------------------------------------
+		// エラーメッセージ登録
+		// ------------------------------------
+
+		if ( !$validity ) {
+			$message = sprintf( ''
+				. __( 'Contains invalid characters or Too many characters.', _TEXT_DOMAIN )
+			);
+		}
+
+		// ------------------------------------
+
+		return $validity;
+	}
+
+	/**
+	 * オプション値の検証を行う。(表示桁数)
+	 *
+	 * @param string $key オプションキー
+	 * @param mixed $value オプション値
+	 * @param string|null $message エラーメッセージ
+	 * @return boolean 有効性の検証結果を返す。(true:有効/false:無効)
+	 */
+	public static function validate_digits( $value, &$message = null )
+	{
+		$validity = empty( $message ) ? true : false ;
+
+		// ------------------------------------
+		// 入力パターン検証
+		// ------------------------------------
+
+		if ( $validity ) {
+			if ( !SELF::is_match_pattern( 'digits', $value ) ) {
+				$validity = false;
+			}
+		}
+
+		// ------------------------------------
+		// エラーメッセージ登録
+		// ------------------------------------
+
+		if ( !$validity ) {
+			$message = sprintf( ''
+				. __( 'Input value is invalid.', _TEXT_DOMAIN )
+				. ' ( ' . __( '1 digit integer. 1~9', _TEXT_DOMAIN ) . ' )'
+			);
+		}
+
+		// ------------------------------------
+
+		return $validity;
+	}
+
+	/**
+	 * オプション値の検証を行う。(メールカウント)
+	 *
+	 * @param string $key オプションキー
+	 * @param mixed $value オプション値
+	 * @param string|null $message エラーメッセージ
+	 * @return boolean 有効性の検証結果を返す。(true:有効/false:無効)
+	 */
+	public static function validate_count( $value, &$message = null )
+	{
+		$validity = empty( $message ) ? true : false ;
+
+		// ------------------------------------
+		// 入力パターン検証
+		// ------------------------------------
+
+		if ( $validity ) {
+			if ( !SELF::is_match_pattern( 'count', $value ) ) {
+				$validity = false;
+			}
+		}
+
+		// ------------------------------------
+		// エラーメッセージ登録
+		// ------------------------------------
+
+		if ( !$validity ) {
+			$message = sprintf( ''
+				. __( 'Input value is invalid.', _TEXT_DOMAIN )
+				. ' ( ' . __( 'Up to 5 digits integer. 0~99999', _TEXT_DOMAIN ) . ' )'
+			);
+		}
+
+		// ------------------------------------
+
+		return $validity;
+	}
+
+	/**
+	 * オプション値の検証を行う。(デイリーカウント)
+	 *
+	 * @param string $key オプションキー
+	 * @param mixed $value オプション値
+	 * @param string|null $message エラーメッセージ
+	 * @return boolean 有効性の検証結果を返す。(true:有効/false:無効)
+	 */
+	public static function validate_daycount( $value, &$message = null )
+	{
+		$validity = empty( $message ) ? true : false ;
+
+		// ------------------------------------
+		// 入力パターン検証
+		// ------------------------------------
+
+		if ( $validity ) {
+			if ( !SELF::is_match_pattern( 'daycount', $value ) ) {
+				$validity = false;
+			}
+		}
+
+		// ------------------------------------
+		// エラーメッセージ登録
+		// ------------------------------------
+
+		if ( !$validity ) {
+			$message = sprintf( ''
+				. __( 'Input value is invalid.', _TEXT_DOMAIN )
+				. ' ( ' . __( 'Up to 5 digits integer. 0~99999', _TEXT_DOMAIN ) . ' )'
+			);
+		}
+
+		// ------------------------------------
+
+		return $validity;
+	}
+
   // ========================================================
 
 	/**
