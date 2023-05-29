63b886c1e502f5ecc7261c233d07ba6838fe3183
nobuntu87 <nobuntu.media.studio@gmail.com>
2023-05-29 18:34:58 +0900

    :up: [upgrade] ライブラリ 「wplib-admin-menu」 を更新
    ■ ライブラリ 「wplib-admin-menu」 を更新した。

--- includes/define.php
+++ includes/define.php
@@ -125,7 +125,7 @@ define( __NAMESPACE__ . '\_FORM_OPTIONS', array(
 
 // WordPress Library「Admin Menu」
 
-define( __NAMESPACE__ . '\_LIB_ADMIN_MENU_VERSION', '2_4_0' );
+define( __NAMESPACE__ . '\_LIB_ADMIN_MENU_VERSION', '2_4_1' );
 
 class_alias(
 	'_Nt\WpLib\AdminMenu\v' . _LIB_ADMIN_MENU_VERSION . '\Admin_Menu_Base',
